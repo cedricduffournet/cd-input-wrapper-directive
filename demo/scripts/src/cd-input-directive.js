aaa1ed8d8e5898c936513d9b0e338ee04e7223fa
cedric <cedric.duffournet@gmail.com>
2014-12-29 16:36:48 +0100

    add input directive

---
+++ demo/scripts/src/cd-input-directive.js
@@ -0,0 +1,50 @@
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc directive
+   * @name cd.ui.inputWrapper.directive:cdInput
+   * @restrict A
+   *
+   * @description
+   * Add class on input if not set
+   *
+   *
+   * @toDo
+   * Write test
+   */
+  angular
+    .module('cd.ui.inputWrapper')
+    .directive('cdInput', cdInput);
+
+
+  cdInput.$inject = ['ErrorMessages', 'CssForm'];
+  function cdInput(ErrorMessages, CssForm) {
+    var directive = {
+      templateUrl: 'template/input-wrapper.html',
+      restrict: 'A',
+      replace: false,
+      controller: CdInput,
+      controllerAs: 'vm',
+      bindToController: true,
+      scope: {
+
+      },
+      link: linkFunc
+    };
+
+    return directive;
+
+    function linkFunc($scope, element, attrs) {
+
+      if (!angular.isDefined(attrs.class)) {
+        element.addClass(CssForm.input);
+      }
+
+    }
+  }
+
+  function CdInput() {
+  }
+
+}());
