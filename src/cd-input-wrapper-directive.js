f54b1b52302520bcab68183b6b1423fcaec467b3
cedric <cedric.duffournet@gmail.com>
2015-03-30 18:40:52 +0200

    radio management

--- src/cd-input-wrapper-directive.js
+++ src/cd-input-wrapper-directive.js
@@ -1,109 +1,124 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  /**
-   * @ngdoc directive
-   * @name cd.ui.inputWrapper.directive:cdInputWrapper
-   * @restrict A
-   *
-   * @description
-   * Wrap input element in form
-   *
-   * @param {String=} containerFieldClass CSS class for container
-   * @param {String=} containerInputClass CSS class for input container
-   * @param {String=} labelClass CSS class for label
-   * @param {String=} labelTitle title of the label
-   * @param {Boolean=} largeInput is field large ?
-   *
-   * @toDo
-   * Write test
-   */
-  angular
-    .module('cd.ui.inputWrapper', [
-      'cd.ui.inputWrapperTpl'
-    ])
-    .directive('cdInputWrapper', inputWrapper);
-
-
-  inputWrapper.$inject = ['ErrorMessages', 'CssForm'];
-  function inputWrapper(ErrorMessages, CssForm) {
-    var directive = {
-      templateUrl: 'template/input-wrapper.html',
-      restrict: 'A',
-      replace: false,
-      transclude: true,
-      require: '^form',
-      controller: InputWrapperController,
-      controllerAs: 'vm',
-
-      bindToController: true,
-      scope: {
-        containerFieldClass: '@containerFieldClass',
-        containerInputClass: '@containerInputClass',
-        labelClass: '@labelClass',
-        labelTitle: '@labelTitle',
-        largeInput: '=largeInput',
-        helpTranslateId:'@helpTranslateId',
-        inputGroup:'=inputGroup'
-      },
-      link: linkFunc
-    };
-
-    return directive;
-
-    function linkFunc($scope, element, attrs, form, transclude) {
+    /**
+     * @ngdoc directive
+     * @name cd.ui.inputWrapper.directive:cdInputWrapper
+     * @restrict A
+     *
+     * @description
+     * Wrap input element in form
+     *
+     * @param {String=} containerFieldClass CSS class for container
+     * @param {String=} containerInputClass CSS class for input container
+     * @param {String=} labelClass CSS class for label
+     * @param {String=} labelTitle title of the label
+     * @param {Boolean=} largeInput is field large ?
+     *
+     * @toDo
+     * Write test
+     */
+    angular
+        .module('cd.ui.inputWrapper', [
+            'cd.ui.inputWrapperTpl'
+        ])
+        .directive('cdInputWrapper', inputWrapper);
 
 
-      var typeInput = '';
+    inputWrapper.$inject = ['ErrorMessages', 'CssForm'];
+    function inputWrapper(ErrorMessages, CssForm) {
+        var directive = {
+            templateUrl: 'template/input-wrapper.html',
+            restrict: 'A',
+            replace: false,
+            transclude: true,
+            require: '^form',
+            controller: InputWrapperController,
+            controllerAs: 'vm',
 
-      if(!angular.isDefined($scope.vm.helpTranslateId)){
-        $scope.vm.helpTranslateId='';
-      }
+            bindToController: true,
+            scope: {
+                containerFieldClass: '@containerFieldClass',
+                containerInputClass: '@containerInputClass',
+                labelClass: '@labelClass',
+                labelTitle: '@labelTitle',
+                largeInput: '=largeInput',
+                helpTranslateId: '@helpTranslateId',
+                inputGroup: '=inputGroup'
+            },
+            link: linkFunc
+        };
 
-      //console.log($scope.vm.helpTranslateId);
-      //to get the element name
-      transclude(function (clone) {
-        var els = Array.prototype.slice.call(clone);
-        els = els.filter(function (element) {
-          return element.nodeType !== Node.TEXT_NODE;
-        });
+        return directive;
 
-        var myELem =  angular.element(els[0]);
-        $scope.vm.name = myELem.attr('name');
-        $scope.vm.required = (myELem.attr('ng-required') || els[0].required);
-        typeInput = myELem.attr('type');
-      });
-
-      $scope.vm.form = form;
-      $scope.vm.field = form[$scope.vm.name];
-      $scope.vm.errorMessages = ErrorMessages;
+        function linkFunc($scope, element, attrs, form, transclude) {
 
 
-      if (!angular.isDefined($scope.vm.containerFieldClass)) {
-        $scope.vm.containerFieldClass = CssForm.wrapperField;
-        if (angular.isDefined($scope.vm.largeInput) && $scope.vm.largeInput) {
-          $scope.vm.containerFieldClass += ' ' + CssForm.groupInputLarge;
+            var typeInput = '';
+
+            if (!angular.isDefined($scope.vm.helpTranslateId)) {
+                $scope.vm.helpTranslateId = '';
+            }
+
+            //console.log($scope.vm.helpTranslateId);
+            //to get the element name
+            transclude(function (clone) {
+
+
+                var els = Array.prototype.slice.call(clone);
+
+                els = els.filter(function (element) {
+                    return element.nodeType !== Node.TEXT_NODE;
+                });
+
+
+                var myELem = angular.element(els[0]);
+
+                if (myELem[0].nodeName == 'LABEL') {
+                    els = Array.prototype.slice.call(els[0].childNodes);
+                    els = els.filter(function (element) {
+                        return element.nodeType !== Node.TEXT_NODE;
+                    });
+                }
+
+                myELem = angular.element(els[0]);
+
+                //console.log(els[0]);
+                $scope.vm.name = myELem.attr('name');
+                $scope.vm.required = (myELem.attr('ng-required') || els[0].required);
+                typeInput = myELem.attr('type');
+            });
+
+            $scope.vm.form = form;
+            $scope.vm.field = form[$scope.vm.name];
+            $scope.vm.errorMessages = ErrorMessages;
+
+
+            if (!angular.isDefined($scope.vm.containerFieldClass)) {
+                $scope.vm.containerFieldClass = CssForm.wrapperField;
+                if (angular.isDefined($scope.vm.largeInput) && $scope.vm.largeInput) {
+                    $scope.vm.containerFieldClass += ' ' + CssForm.groupInputLarge;
+                }
+            }
+
+            if (!angular.isDefined($scope.vm.containerInputClass)) {
+                switch (typeInput) {
+                    case 'number':
+                        $scope.vm.containerInputClass = CssForm.wrapperInputNumber;
+                        break;
+                    default:
+                        $scope.vm.containerInputClass = CssForm.wrapperInput;
+                }
+            }
+
+            if (!angular.isDefined($scope.vm.labelClass)) {
+                $scope.vm.labelClass = CssForm.label;
+            }
+
         }
-      }
-
-      if (!angular.isDefined($scope.vm.containerInputClass)) {
-        switch (typeInput) {
-          case 'number':
-            $scope.vm.containerInputClass = CssForm.wrapperInputNumber;
-            break;
-          default:
-            $scope.vm.containerInputClass = CssForm.wrapperInput;
-        }
-      }
-
-      if (!angular.isDefined($scope.vm.labelClass)) {
-        $scope.vm.labelClass = CssForm.label;
-      }
-
     }
-  }
 
-  function InputWrapperController() {
-  }
+    function InputWrapperController() {
+    }
 
 }());
