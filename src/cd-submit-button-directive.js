fdb06d7fe6f6da4ef03a29587f6ecacb3fc1ae74
cedric <cedric.duffournet@gmail.com>
2015-04-12 19:10:27 +0200

    button close

--- src/cd-submit-button-directive.js
+++ src/cd-submit-button-directive.js
@@ -1,68 +1,68 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  /**
-   * @ngdoc directive
-   * @name cd.ui.inputWrapper.directive:cdSubmitButton
-   * @restrict A
-   *
-   * @description
-   * Wrap input element in form
-   *
-   * @param {String=} containerFieldClass CSS class for container
-   * @param {String=} containerSubmitClass CSS class for button container
-   * @param {String=} buttonClass CSS class button
-   * @param {String=} title title of the button
-   *
-   * @toDo
-   * Write test
-   */
-  angular
-    .module('cd.ui.inputWrapper')
-    .directive('cdSubmitButton', submitButton);
+    /**
+     * @ngdoc directive
+     * @name cd.ui.inputWrapper.directive:cdSubmitButton
+     * @restrict A
+     *
+     * @description
+     * Wrap input element in form
+     *
+     * @param {String=} containerFieldClass CSS class for container
+     * @param {String=} containerSubmitClass CSS class for button container
+     * @param {String=} buttonClass CSS class button
+     * @param {String=} title title of the button
+     *
+     * @toDo
+     * Write test
+     */
+    angular
+        .module('cd.ui.inputWrapper')
+        .directive('cdSubmitButton', submitButton);
 
 
-  submitButton.$inject = ['CssForm'];
-  function submitButton(CssForm) {
-    var directive = {
-      templateUrl: 'template/submit-button.html',
-      restrict: 'E',
-      replace: true,
-      require: '^form',
-      controller: SubmitButtonController,
-      controllerAs: 'vm',
-      bindToController: true,
-      scope: {
-        containerFieldClass: '@containerFieldClass',
-        containerSubmitClass: '@containerSubmitClass',
-        buttonClass: '@buttonClass',
-        title: '@title',
-        withCloseButton:'=withCloseButton'
-      },
-      link: linkFunc,
-      transclude:true
-    };
+    submitButton.$inject = ['CssForm'];
+    function submitButton(CssForm) {
+        var directive = {
+            templateUrl: 'template/submit-button.html',
+            restrict: 'E',
+            replace: true,
+            require: '^form',
+            controller: SubmitButtonController,
+            controllerAs: 'vm',
+            bindToController: true,
+            scope: {
+                containerFieldClass: '@containerFieldClass',
+                containerSubmitClass: '@containerSubmitClass',
+                buttonClass: '@buttonClass',
+                title: '@title',
+                withCloseButton: '=withCloseButton'
+            },
+            link: linkFunc,
+            transclude: true
+        };
 
-    return directive;
+        return directive;
 
-    function linkFunc($scope) {
+        function linkFunc($scope) {
 
-      if (!angular.isDefined($scope.vm.containerFieldClass)) {
-        $scope.vm.containerFieldClass = CssForm.wrapperField;
-      }
+            if (!angular.isDefined($scope.vm.containerFieldClass)) {
+                $scope.vm.containerFieldClass = CssForm.wrapperField;
+            }
 
-      if (!angular.isDefined($scope.vm.containerInputClass)) {
-        $scope.vm.containerSubmitClass = CssForm.wrapperSubmit;
-      }
+            if (!angular.isDefined($scope.vm.containerInputClass)) {
+                $scope.vm.containerSubmitClass = CssForm.wrapperSubmit;
+            }
 
-      if (!angular.isDefined($scope.vm.labelClass)) {
-        $scope.vm.buttonClass = CssForm.submit;
-      }
-
+            if (!angular.isDefined($scope.vm.labelClass)) {
+                $scope.vm.buttonClass = CssForm.submit;
+            }
+            $scope.vm.cancel = $scope.$parent.cancel;
+        }
     }
-  }
 
-  function SubmitButtonController() {
-  }
+    function SubmitButtonController() {
+    }
 
 }());
